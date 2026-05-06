c7387f40df1df54921ca663636618af1ca881c8e
zainabalzaimoor <alzaimoorzainab@gmail.com>
2026-05-06 19:10:45 +0300

    inital commits

---
+++ src/main/java/com/findit/lostfound_system/LostfoundSystemApplication.java
@@ -0,0 +1,13 @@
+package com.findit.lostfound_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostfoundSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostfoundSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/findit/lostfound_system/LostfoundSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findit.lostfound_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostfoundSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
