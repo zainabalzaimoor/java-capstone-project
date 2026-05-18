cc8fe4f20773a9645e801cb48c85045eb296c30a
zainabalzaimoor <alzaimoorzainab@gmail.com>
2026-05-18 21:40:38 +0300

    feat: add the needed DTOs ItemRequest and ItemResponse

--- src/main/java/com/findit/lostfoundsystem/dto/ItemRequestDTO.java
+++ src/main/java/com/findit/lostfoundsystem/dto/ItemRequestDTO.java
@@ -1,4 +1,19 @@
 package com.findit.lostfoundsystem.dto;
 
+import com.findit.lostfoundsystem.enums.ItemType;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class ItemRequestDTO {
+
+    private String title;
+    private String description;
+    private String category;
+    private String location;
+    private LocalDate date;
+    private String itemImageUrl;
+    private ItemType type;
+
 }
