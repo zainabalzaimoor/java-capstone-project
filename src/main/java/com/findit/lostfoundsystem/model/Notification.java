5bd4fccd8f6f1ff999708b7cc037c85caca26b21
zainabalzaimoor <alzaimoorzainab@gmail.com>
2026-05-23 03:04:40 +0300

    feat: add Notification entity with user, message, type, isRead fields

--- src/main/java/com/findit/lostfoundsystem/model/Notification.java
+++ src/main/java/com/findit/lostfoundsystem/model/Notification.java
@@ -1,15 +1,14 @@
 package com.findit.lostfoundsystem.model;
 
+import com.findit.lostfoundsystem.enums.NotificationType;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
+@Builder
 @Entity
 @Getter
 @Setter
@@ -17,18 +16,30 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Table(name = "notifications")
 public class Notification {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",  nullable = false)
     private User user;
 
+    @Column(columnDefinition = "TEXT", nullable = false)
     private String message;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationType type;
+
+    @Column(name = "is_read")
+    @Builder.Default
     private boolean isRead = false;
 
+    @Column(name = "related_item_id")
+    private Long relatedItemId;
+
     @CreationTimestamp
+    @Column(updatable = false)
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
